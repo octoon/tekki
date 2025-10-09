9c9107b9391852b184228ed629f22e756395d1ad
jstzwj <1103870790@qq.com>
2025-10-10 03:43:14 +0800

    clean old code

--- include/tekki/core/common.h
+++
@@ -1,9 +0,0 @@
-// tekki - C++ port of kajiya renderer
-// Copyright (c) 2025 tekki Contributors
-// SPDX-License-Identifier: MIT OR Apache-2.0
-
-// Common header file for C compatibility
-
-#pragma once
-
-#include "common.hpp"
--- tests/test_main.cpp
+++
@@ -1,6 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch_session.hpp>
-
-// This file provides the main() entry point for all Catch2 tests.
-// Individual test files only need to include <catch2/catch_test_macros.hpp>
-// and define their TEST_CASE blocks.
